ef4e2813a19ee916b3a029172a4fa1126649976c
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2021-12-28 17:24:06 +0300

    Update factorialLoop.php

--- factorialLoop.php
+++ factorialLoop.php
@@ -1,10 +1,9 @@
 <?php
 
 $number = 5;
-
 $factorial = 1;
-for ($i = 2;$i <= $number;$i++)
-{
+
+for ($i = 2; $i <= $number; $i++){
     $factorial = $factorial * $i;
 
     // 2   = 1 * 2
@@ -12,9 +11,26 @@ for ($i = 2;$i <= $number;$i++)
     // 24  = 6 * 4
     // 120 = 24 * 5
     
-
-    //echo $factorial."<br>";
-    
+    //echo $factorial."<br>";  
 }
 echo $factorial;
+
+
+
+$number = 5;
+$factorial = 1;
+
+for ($i = 2;$i <= $number;$i++){
+    $factorial = $factorial * $i;
+
+    // 2   = 1 * 2
+    // 6   = 2 * 3
+    // 24  = 6 * 4
+    // 120 = 24 * 5
+
+    //echo $factorial."<br>"; 
+}
+echo $factorial;
+
+
 ?>
