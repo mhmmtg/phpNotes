52a7c309a1659e402686439ba802a3536bcc6315
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2021-12-31 12:50:14 +0300

    Create evenOrOdd.php

---
+++ evenOrOdd.php
@@ -0,0 +1,20 @@
+<?php
+
+$start = 1;
+$stop = 25;
+
+while ($start < $stop) {
+    $start++;
+
+    if ($start % 2 == 0) {
+        echo $start . "<br>";
+    }
+}
+
+$number = range(0, 5);
+print_r($number);
+
+$number2 = range(0, 15, 2);
+print_r($number2);
+
+?>
