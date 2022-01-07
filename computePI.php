0fb99a29836cdd0ed1fb3ad87eb0e7740850a53b
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2022-01-07 16:13:40 +0300

    Update computePI.php

--- computePI.php
+++ computePI.php
@@ -3,12 +3,12 @@
 $piPlus = 1;
 $piMinus = 1.0 / 3;
 
-for ($i = 4; $i < 100; $i += 4) {
+$attempt = 500;
+
+for ($i = 4; $i < $attempt; $i += 4) {
     $piPlus = $piPlus + 1.0 / ($i + 1); // 1 + 1/5...
     $piMinus = $piMinus + 1.0 / ($i + 3); // 1/3 + 1/7...
 }
-echo $piPlus . "<br>";
-echo $piMinus . "<br>";
 echo 4 * ($piPlus - $piMinus);
 
 ?>
