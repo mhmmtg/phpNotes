c200989ab99314bbfd48c9e64923a13df7742096
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2022-01-23 13:29:08 +0300

    Update myNotes.php

--- myNotes.php
+++ myNotes.php
@@ -2,3 +2,12 @@
 <?=$a; ?>
 
 //===
+
+In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
+However; all variable names are case-sensitive!
+
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!"; //php in double quote
+?>
+In PHP 7, type declarations were added.
