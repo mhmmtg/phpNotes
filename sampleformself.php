dd023a22a3ae4287c685809a33243b57a3eae1b5
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2022-01-24 13:20:16 +0300

    Update sampleformself.php

--- sampleformself.php
+++ sampleformself.php
@@ -8,26 +8,85 @@
 <?php
 $formTitle = $firstParagraph = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
+//if(empty($formTitle)){}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $formTitle = inputCheckher($_POST['formtitle']);
     $firstParagraph = inputCheckher($_POST['firstparag']);
 }
 
-//if(empty($formTitle)){}
-function inputCheckher($input)
-{
+try {
+    $db = new PDO("mysql:host=localhost; dbname=test; charset=utf8", "root", "");
+} catch (Exception $e) {
+    echo $e;
+}
+
+selectOne();
+selectAll();
+insertQuery();
+updateQuery();
+deleteQuery();
+
+
+function inputCheckher($input) {
     $input = trim($input);
     $input = stripslashes($input);
     $input = htmlspecialchars($input);
     return $input;
 }
 
+function selectOne() {
+    global $db;
+    $id = 3;
+    $getOneQuery = $db->query("SELECT * FROM deneme WHERE den_id = $id")->fetch(PDO::FETCH_ASSOC);
+
+    //$getOneQuery = $db->query("SELECT * FROM deneme WHERE den_id = '{$id}'")->fetch(PDO::FETCH_ASSOC);
+    //print_r($getOneQuery);
+
+    echo $getOneQuery['den_ad'];
+}
+
+function selectAll() {
+    global $db;
+    $getAllQuery = $db->query("SELECT * FROM deneme", PDO::FETCH_ASSOC);
+    echo $getAllQuery->rowCount() . "<br>"; //if
+    foreach ($getAllQuery as $row) {
+        echo $row['den_ad'] . "<br>";
+    }
+}
+
+function insertQuery() {
+    global $db;
+    $insertQuery = $db->prepare("INSERT INTO deneme(den_ad) values (?)");
+    //$insertQuery = $db->prepare("INSERT INTO deneme SET den_ad = ?");
+
+    $insertOK = $insertQuery->execute(array("deneme"));
+
+    //if ($insertQuery->rowCount()){}
+    if ($insertOK) {
+        $last_id = $db->lastInsertId();
+        echo "insertOK";
+        echo $last_id;
+    }
+}
+
+function updateQuery() {
+    global $db;
+    $updateQuery = $db->prepare("UPDATE deneme SET den_ad = ? WHERE den_id = ?");
+    $updateQuery->execute(array("denemeyeni", "3"));
+}
+
+function deleteQuery() {
+    global $db;
+    $deleteQuery = $db->prepare("DELETE FROM deneme WHERE den_id = ?");
+    $deleteQuery->execute(array(5)); //$delete = ...
+}
+
+$db = null;
 ?>
 
 <h1>Php Sample Form</h1>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+<form action="<?php echo inputCheckher($_SERVER["PHP_SELF"]) ?>" method="post">
     <label for="formtitle">Form Title:</label><br>
     <input type="text" name="formtitle" placeholder="enter form title"><br><br>
 
@@ -38,8 +97,10 @@ function inputCheckher($input)
 </form>
 
 <h1>Your Input</h1>
-    <h2>Form Title: <?=$formTitle ?></h2>
-    <h3>First Paragraph: <?=$firstParagraph ?></h3>
+<h2>Form Title: <?= $formTitle ?></h2>
+<h3>First Paragraph: <?= $firstParagraph ?></h3>
 
 </body>
 </html>
+
+
